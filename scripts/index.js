5682b0b9116e72c3c129d48f674603f6000194b5
Vladimir <derekpro@mail.ru>
2022-11-12 00:16:36 +0300

    Исправлен баг с переполнением контента

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const popUpElement = document.querySelector('.pop-up');
 const popUpExitButtonElement = popUpElement.querySelector('.pop-up__exit');
 const profileEditButtonElement = document.querySelector('.profile__edit-button');
 const popUpFormElement = document.querySelector('.pop-up__form');
-const nameInput = document.querySelector('.pop-up__name-input');
-const jobInput = document.querySelector('.pop-up__job-input');
+const nameInput = document.querySelector('.pop-up__user-input_info_name');
+const jobInput = document.querySelector('.pop-up__user-input_info_job');
 const nameProfile = document.querySelector('.profile__name-profile');
 const jobProfile = document.querySelector('.profile__subname-profile');
 const formElement = popUpElement.querySelector('.pop-up__form');
@@ -16,26 +16,27 @@ const openPopUp = function () {
 }
 const closePopUp = function () {
     popUpElement.classList.remove('pop-up_active');
-    formElement.reset();
+    
 }
-const closePopUpByClickOnOverlay = function (event) {
-    if (event.target !== event.currentTarget) {
-        return;
-    }
-    closePopUp()
-    formElement.reset();
-}
+// const closePopUpByClickOnOverlay = function (event) {
+//     if (event.target !== event.currentTarget) {
+//         return;
+//     }
+//     closePopUp()
+//     formElement.reset();
+// }
 
 function formSubmitHandler(event) {
     event.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
+    formElement.reset();
     closePopUp()
 }
 
 profileEditButtonElement.addEventListener('click', openPopUp);
 popUpExitButtonElement.addEventListener('click', closePopUp);
-popUpElement.addEventListener('click', closePopUpByClickOnOverlay);
+// popUpElement.addEventListener('click', closePopUpByClickOnOverlay);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
